11441b773692f63d3dab21e66773dd4e8f15e38a
Aaron DuChateau <chateauconcept@gmail.com>
2021-04-23 10:53:31 -0400

    force build test

---
+++ client/buildx/precache-manifest.432f490d71ec04a0e45a3c8f986c8ffa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d642ef50a0c94969e84ef6fe64c835ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb9904a862b58ee4a9be",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c9e8d464b4b0350b4c34",
+    "url": "/static/css/main.cb204a48.chunk.css"
+  },
+  {
+    "revision": "fb9904a862b58ee4a9be",
+    "url": "/static/js/2.f689f7fb.chunk.js"
+  },
+  {
+    "revision": "936dad2f235ed09abecd4e1b261bf649",
+    "url": "/static/js/2.f689f7fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9e8d464b4b0350b4c34",
+    "url": "/static/js/main.bfc6409c.chunk.js"
+  },
+  {
+    "revision": "5caf021399d84750a193",
+    "url": "/static/js/runtime-main.d3fcc571.js"
+  }
+]);
